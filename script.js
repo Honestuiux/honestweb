619ed13e3788d81f8efae50dba2af55ac412abd1
maryperpetual <maryperpetual123@gmail.com>
2023-10-03 14:45:18 +0530

    done

---
+++ script.js
@@ -0,0 +1,23 @@
+// CURSOR
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 250);
+});
+
+
+// PRELOADER
+window.addEventListener("load", function() {  
+    setTimeout(function() {
+        document.querySelector(".preloader").style.display = "none";
+        document.querySelector(".content").style.display = "block";
+    }, 3000);
+});
+
