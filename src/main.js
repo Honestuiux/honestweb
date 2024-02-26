0e932bd62e0ea036bf6dbc87ea4fbb4d133776e0
maryperpetual <maryperpetual123@gmail.com>
2024-02-26 16:26:38 +0530

    3d

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { Application } from '@splinetool/runtime';
+
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/hZhswtstNT1DqB6X/scene.splinecode');
+ 
